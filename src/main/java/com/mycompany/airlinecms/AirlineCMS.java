5b16e8ac6bc77fe0041030ec1f54db619752b657
Raoul <76117365+rasimaxx@users.noreply.github.com>
2023-02-28 09:57:34 +0100

    Create JForms

---
+++ src/main/java/com/mycompany/airlinecms/AirlineCMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinecms;
+
+/**
+ *
+ * @author signe
+ */
+public class AirlineCMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
